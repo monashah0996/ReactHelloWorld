bdcf1c1d872baf6143d02c98ca869108a9d382a3
monas0996 <monashah0996@gmail.com>
2023-07-20 13:14:00 -0400

    code to describe how elements are rendered

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function tick(){
+  const element = (
+    <div>
+      <h1> We are in PST Timezone</h1>
+      <h2>CurrentTime: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+      { element }   
+    </React.StrictMode>
+  );
+}
+
+setInterval(tick,1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
